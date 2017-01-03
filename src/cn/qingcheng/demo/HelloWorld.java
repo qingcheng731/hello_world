dc503fd98c1980ea9b541c3cd0d5b5f3ca162c10
qingcheng731 <958617315@qq.com>
2017-01-03 11:07:10 +0800

    Initial commit

---
+++ src/cn/qingcheng/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.qingcheng.demo;
+
+/**
+ * Created by wangzhoucheng on 2017/01/03 0003.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
